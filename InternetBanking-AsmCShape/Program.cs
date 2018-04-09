916d7bb5b6829e0438ff909287822c924351ed15
anhnq193 <cafedang@gmail.com>
2018-04-09 17:15:05 +0700

    init project

--- InternetBanking-AsmCShape/Program.cs
+++ InternetBanking-AsmCShape/Program.cs
@@ -10,6 +10,8 @@ namespace InternetBanking_AsmCShape
     {
         static void Main(string[] args)
         {
+            Console.Write("hello");
+            Console.Read();
         }
     }
 }
