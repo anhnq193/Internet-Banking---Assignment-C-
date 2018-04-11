965ef4f63a904083f618ca64fc04f63053251e17
anhnq193 <cafedang@gmail.com>
2018-04-11 11:33:52 +0700

    complete project stucture and write main menu in Controller/MainController.cs

---
+++ InternetBanking-AsmCShape/Controller/TransactionFlows/AccInfoFlow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.TransactionFlows
+{
+    class AccInfoFlow
+    {
+        public static void AccountInfo(string accNum)
+        {
+
+        }
+    }
+}
---
+++ InternetBanking-AsmCShape/Controller/TransactionFlows/TranHistoryFlow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.TransactionFlows
+{
+    class TranHistoryFlow
+    {
+        public static void TranHistory(string accNum)
+        {
+
+        }
+    }
+}
