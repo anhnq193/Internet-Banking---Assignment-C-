aa7de655ff5ffecf06032388ccbc91d117e232dc
Trung Viet <tran.trungviet19@gmail.com>
2018-04-17 20:26:51 +0700

    Update entities transaction

--- InternetBanking-AsmCShape/Entities/Transaction.cs
+++ InternetBanking-AsmCShape/Entities/Transaction.cs
@@ -3,77 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controller;
 
 namespace Entities
 {
     class Transaction
     {
-        private int ID;
+        private int id;
         private long timeCreated;
-        private string fromNumAcc ;
+        private string fromNumAcc;
         private string toNumAcc;
         private long amount;
         private long timeSuccess;
 
-
-        public int ID1 { get => ID; set => ID = value; }
-        public long TimeCreated { get => timeCreated; set => timeCreated = value; }
-        public string FromNumAcc { get => fromNumAcc; set => fromNumAcc = value; }
-        public string ToNumAcc { get => toNumAcc; set => toNumAcc = value; }
-        public long Amount { get => amount; set => amount = value; }
-        public long TimeSuccess { get => timeSuccess; set => timeSuccess = value; }
-       
-
-        static DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        static long ms = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
-
-        public Transaction()
+        public Transaction(string fromNumAcc, string toNumAcc, long amount)
         {
-        }
-        
-
-        public Transaction(long timeCreated, string fromNumAcc, string toNumAcc, long amount)
-        {
-            this.timeCreated = ms;
             this.fromNumAcc = fromNumAcc;
             this.toNumAcc = toNumAcc;
             this.amount = amount;
+            this.timeCreated = TimeUltility.CurrentEpochMilli;
         }
 
-        public DateTime getTimeCreated() {
-            long dateNumber = ms;
-            long beginTicks = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
-            DateTime dateValue = new DateTime(beginTicks + dateNumber * 10000);
-
-            return dateValue;
+        public Transaction(int id, long timeCreated, string fromNumAcc, string toNumAcc, long amount, long timeSuccess)
+        {
+            this.id = id;
+            this.timeCreated = timeCreated;
+            this.fromNumAcc = fromNumAcc;
+            this.toNumAcc = toNumAcc;
+            this.amount = amount;
+            this.timeSuccess = timeSuccess;
         }
 
-        public DateTime getTimeSuccess() {
-            long dateNumber = timeSuccess;
-            long beginTicks = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
-            DateTime dateValue = new DateTime(beginTicks + dateNumber * 10000);
+        public int Id { get => id; }
+        public long TimeCreated { get => timeCreated; }
+        public string FromNumAcc { get => fromNumAcc; }
+        public string ToNumAcc { get => toNumAcc; }
+        public long Amount { get => amount; }
+        public long TimeSuccess { get => timeSuccess; set => timeSuccess = value; }
 
-            return dateValue;
-        }
-
-        public bool isSuccess() {
-            if (timeSuccess == 0)
+        public bool isWithdrawTransection()
+        {
+            if (toNumAcc.Equals(""))
             {
-
-                return false;
-
-            }
-            return true;
-        }
-
-        public bool isWithdrawTransection() {
-            if (toNumAcc == null)
-            {
-
                 return true;
-
             }
             return false;
+        }
+
+        public bool isSuccess()
+        {
+            if (timeSuccess == 0 || timeSuccess <= timeCreated)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
